671bdea73460b084f9abdd102f5fd045200b063b
Kulesh <Victoria.Kulesh@msdecisions.tech>
2025-10-27 14:05:16 +0300

    minor fixed for time profiles

--- R/utils.R
+++ R/utils.R
@@ -24,7 +24,7 @@
 #' @param eta_seq vector of strings. Character vector of parameter names to be plotted (e.g., `c("ka", "Cl")`). If `NULL`, all parameters be included. Default is `NULL`
 #' @param f_scales one of `"fixed"`, `"free"`, `"free_x"`, `"free_y"`. User can specify whether the scales (x and y axes) should be fixed across all panels (`"fixed"`), free for each panel (`"free"`), or free only in one dimension (`"free_x"` or `"free_y"`). Default is `"fixed"`
 #' @param facet_i string. Column name for facet
-#' @param fpath_i string or sg-fit object. If the string is given, the path to `.Rdata` file with sg-fit object is expected
+#' @param fpath_i string or sg-fit object. If the string is given, the path to `.Rdata` or `.json` file with sg-fit object is expected
 #' @param ncores integer. Number of cores used for calculations. Default is 1
 #' @param indiv logical. If `TRUE` uses individual predictions (`"IPRED"`); otherwise uses population predictions (`"PRED"`). Default is `TRUE`
 #' @param inits named vector. Initial conditions of model variables. Default is `NULL`
@@ -47,7 +47,6 @@
 #' @param npop integer. Number of population replicates. Default is 1
 #' @param nsub integer. Number of subjects sampled per population (omega/sigma matrices per ID). Default is 1
 #' @param omega named mztrix or vector. Matrix
-#' @param output vector of strings. The names of model variables to output. If `NULL`, all variables will be outputed. Default is `NULL`
 #' @param outputs vector of strings. Names of the model variabeles to output. If `NULL`, all varaibles returned. Default is `NULL`
 #' @param plot_type Character. Type of plot to produce:
 #'   * `"DIST"` (default) - histogram of individual parameters,
