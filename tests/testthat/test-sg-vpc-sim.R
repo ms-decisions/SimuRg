50986aa777157d125ac5ffdb8cf5d666c4f77f50
amelnikova <alina.melnikova@msdecisions.tech>
2025-12-04 17:02:35 +0300

    add previous test to test-sg-vpc-sim

--- tests/testthat/test-sg-vpc-sim.R
+++ tests/testthat/test-sg-vpc-sim.R
@@ -1,7 +1,48 @@
-## Author: Melnikova Alina
-## First created: 2025-12-04
-## Description: formal testing of sg-vpc-sim function and its helpers
-## Keywords: SimuRg, sg-vpc-sim, goodness-of-fit
+test_that("sg_sim_vps works ", {
+  mod_fin <- RxODE({
+    # Doses in mg
+    # Time in hours
+
+    ### Parameter values
+    # Typical
+    Cl_pop = 5;
+    V_pop = 180;
+
+    ka_pop = 6;
+
+
+    # Random effects
+    omega_Cl = 0;
+    omega_V = 0;
+    omega_ka = 0;
+
+    # Residual error
+    b = 0;
+
+    ### Parameters
+    Cl = Cl_pop * exp(omega_Cl);
+    V = V_pop * exp(omega_V);
+    ka = ka_pop * exp(omega_ka);
+
+    ### Explicit functions
+    Cc = Ac/V;                 # nmol/L
+
+    ### Initial conditions
+    Ad(0) = 0;          # mg
+    Ac(0) = 0;          # mg
+
+    ### ODEs
+    d/dt(Ad) = - ka*Ad;
+    d/dt(Ac) = ka*Ad - Cl*Cc ;
+
+    CHECKRUV = b;
+    Cc_ResErr = Cc + b*Cc;
+  })
+  res <- sg_vpc_sim(obj1, mod_fin, output = "Cc")
+  expect_equal(res %>% pull(ID) %>% unique() %>% length(), 100)
+  expect_equal(res %>% pull(TIME) %>% unique() %>% length(),
+               obj1$SDTAB$TIME %>% unique() %>% length())
+})
 
 # Helper function to create mock sg_fit object
 create_mock_sg_fit <- function() {
