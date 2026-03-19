e11e712754f3301ce55901469229395e86043938
amelnikova <alina.melnikova@msdecisions.tech>
2026-03-19 14:24:06 +0300

    add json data object with covariates for sg_covsens_sim to sysdata.rda

--- data-raw/data-gfo-4cov.R
+++ data-raw/data-gfo-4cov.R
@@ -3,6 +3,7 @@
 ## Description: functions for external data preparation
 ## Keywords: SimuRg, external data
 
-gfo4cov <- read.csv(file.path("inst", "extdata",  "datasets", "run_4cov_smrg_results_mod.json"))
-
-usethis::use_data(gfo4cov, overwrite = TRUE)
+#gfo4cov <- read.csv(file.path("inst", "extdata",  "datasets", "run_4cov_smrg_results_mod.json"))
+fpath <- system.file("extdata",  "simurg_object", "run_4cov_smrg_results_mod.json", package = "SimuRg")
+gfo4cov <- read_smrg_obj(fpath)
+usethis::use_data(gfo4cov, overwrite = TRUE, internal = TRUE)
